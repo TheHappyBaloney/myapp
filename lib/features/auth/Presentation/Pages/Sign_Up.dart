f873679e6663404a0f7a4f195df24c8a02b850ed
a.chatterjeeofficial2003 <a.chatterjeeofficial2003@gmail.com>
2024-03-26 20:01:45 +0000

    Repsoitory Set up

---
+++ lib/features/auth/Presentation/Pages/Sign_Up.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: file_names, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage>{
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
